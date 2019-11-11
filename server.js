f05d1977f664645d0e63cc03244a8714f7c6b30c
Radha Bh <rsoujanya.bh@gmail.com>
2019-11-12 06:49:10 +1100

    updated prod directories

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path =require('path')
 
-app.use(express.static(_dirname + '/dist/musicmanagerapp'))
+app.use(express.static(__dirname + '/dist/musicmanagerapp'))
 app.listen(process.env.PORT||8080)
 
 //path location
 app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname+'/dist/musicmanagerapp/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/musicmanagerapp/index.html'))
 })
